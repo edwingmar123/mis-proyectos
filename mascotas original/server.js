4a261972f86bb3ad5aa94407930f4410e76c89f9
Edwing <macdo796@gmail.com>
2024-12-12 01:28:53 -0500

    mio

--- mascotas original/server.js
+++ mascotas original/server.js
@@ -1,10 +1,12 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("data.json");
+const router = jsonServer.router('data/data.json'); // Ajusta la ruta si tu data.json está en otra ubicación
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running on http://localhost:3000");
+
+const PORT = process.env.PORT || 3000; // Render usará su puerto asignado
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
