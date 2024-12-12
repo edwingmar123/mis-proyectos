20f0fffc1a5759b6276ef76212a95388d83356e7
Edwing <macdo796@gmail.com>
2024-12-12 11:26:09 -0500

    nada

--- mascotas original/constants.js
+++
@@ -1,3 +0,0 @@
-export const url_mascotas = "http://localhost:3000/macotas";
-export const url_users = "http://localhost:3000/users";
-export const url_favoritos = "http://localhost:3000/favoritos";
--- mascotas original/getData.js
+++
@@ -1,9 +0,0 @@
-export const getData = async (url) => {
-  try {
-    const reps = await fetch(url);
-    const data = await reps.json();
-    return data;
-  } catch (error) {
-    alert(error.message);
-  }
-};
